98750f16ca981906afe30edb488f759afb0f5aaa
batuhanbilginn <batuhanbilginn@gmail.com>
2023-01-20 01:01:02 +0300

    revalidate api endpoint added

---
+++ pages/api/revalidate.ts
@@ -0,0 +1,29 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  // Check for secret to confirm this is a valid request
+  if (req.query.secret !== process.env.ADMIN_TOKEN) {
+    return res.status(401).json({ message: "Invalid token" });
+  }
+
+  const { path } = req.body;
+
+  if (!path) {
+    return res.status(400).json({ message: "Missing path" });
+  }
+
+  try {
+    // This should be the actual path not a rewritten path
+    // e.g. for "/blog/[slug]" this should be "/blog/post-1"
+    await res.revalidate(path);
+    return res.json({ revalidated: true });
+  } catch (err) {
+    // If there was an error, Next.js will continue
+    // to show the last successfully generated page
+    return res.status(500).send("Error revalidating");
+  }
+}
